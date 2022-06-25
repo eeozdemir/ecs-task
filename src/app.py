687ed63638b095d05ff03114dd150710e6810f3f
Hamid Emamian <emami.he@gmail.com>
2022-06-25 18:33:58 +0300

    code test2

--- src/app.py
+++ src/app.py
@@ -9,12 +9,14 @@ def main():
     print("hello world")
     print("initializing client")
     client=boto3.client("ecs", region_name="us-east-1")
+    clusters = client.list_clusters()
+    cluster_name = clusters['clusterArns'][1]
 
     response = client.update_service(
             service = "arn:aws:ecs:us-east-1:132248825767:service/hamid-testlab-staging/nginx",
             desiredCount = 0
         )
-        
+
     print(json.dumps(response, indent=4, default=str))
 
 if __name__ == "__main__":
